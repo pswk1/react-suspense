1de832cbccf2f64c4d7ae74df6cc0cb1f53c4f52
Peter Kang <peeterkang@gmail.com>
2021-08-20 15:21:12 -0700

    feat(react-suspense): initial setup
    - CRA setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+const App = () => {
+  <div className='container my-5'>
+    <h1>Hello World</h1>
+  </div>
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// concurrent mode
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
